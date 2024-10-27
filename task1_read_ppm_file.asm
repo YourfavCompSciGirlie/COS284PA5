58339c74ebb6a4b0a8ab90d49adde3bcfb42df6a
Malaika <u23618583@tuks.co.za>
2024-10-27 22:55:48 +0200

    Updated files, task 1 and task 3 need help

--- task1_read_ppm_file.asm
+++ task1_read_ppm_file.asm
@@ -1,9 +1,9 @@
 ; ==========================
 ; Group member 01: Yohali_Malaika_Kamangu_u23618583
-; Group member 02: Name_Surname_student-nr
-; Group member 03: Name_Surname_student-nr
-; Group member 04: Name_Surname_student-nr
-; Group member 05: Name_Surname_student-nr
+; Group member 02: Mpho_Martha_Siminya_u21824241
+; Group member 03: Thabiso_Mncube_u22617362
+; Group member 04: Amantle_Temo_u23539764
+; Group member 05: David_Kalu_u23534975
 ; ==========================
 
 extern malloc
--- task2_compute_cdf_values.asm
+++ task2_compute_cdf_values.asm
@@ -1,10 +1,11 @@
 ; ==========================
 ; Group member 01: Yohali_Malaika_Kamangu_u23618583
-; Group member 02: Name_Surname_student-nr
-; Group member 03: Amantle_Temo_u23539764
-; Group member 04: David_Kalu_u23534975
-; Group member 05: Name_Surname_student-nr
-; =========================
+; Group member 02: Mpho_Martha_Siminya_u21824241
+; Group member 03: Thabiso_Mncube_u22617362
+; Group member 04: Amantle_Temo_u23539764
+; Group member 05: David_Kalu_u23534975
+; ==========================
+
 section .data
     ;needed for calculations
     red   dd 0.299    
